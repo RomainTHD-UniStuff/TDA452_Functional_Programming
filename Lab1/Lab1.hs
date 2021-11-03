503ede866eec9d65f3a5dd7e5537eba9b669b9be
Romain <romain.theodet@hotmail.fr>
2021-11-03 13:01:11 +0100

    change: Last function `powerTest`

--- Lab1/Lab1.hs
+++ Lab1/Lab1.hs
@@ -14,9 +14,7 @@ power n 0  = 1
 power n k  = n * power n (k-1)
 
 -- A ------------------------
--- stepsPower n k gives the number of steps that
--- power n k takes to compute
-
+-- stepsPower n k gives the number of steps that power n k takes to compute
 
 stepsPower :: Integer -> Integer -> Integer
 stepsPower n k | k < 0 = error "power: negative argument"
@@ -25,13 +23,14 @@ stepsPower n k = k + 1
 
 
 -- B -------------------------
--- power1
+-- power1, using product
 
 power1 :: Integer -> Integer -> Integer
 power1 n k | k < 0 = error "power: negative argument"
 power1 n k = product [n | _ <- list]
              where list = [1..k]
 
+-- power1, using replicate
 power1_2 :: Integer -> Integer -> Integer
 power1_2 n k | k < 0 = error "power: negative argument"
 power1_2 n k = product (replicate k' n)
@@ -57,24 +56,22 @@ n = 0 and k = 0 (solution : 1) because it tests the case of k = 0 which leads
 These following tests ensure that if n = 0, the solution is 0, except if k = 0.
 n = 0 and k = 1 (solution : 0)
 n = 0 and k = 2 (solution : 0)
-n = 0 and k = 3 (solution : 0)
+n = 0 and k = 100 (solution : 0)
 
 These following tests ensure that if n = 1, the solution is always 1,
  no matter the valuation of k.
 n = 1 and k = 0 (solution : 1)
 n = 1 and k = 1 (solution : 1)
 n = 1 and k = 2 (solution : 1)
-n = 1 and k = 3 (solution : 1)
+n = 1 and k = 100 (solution : 1)
 
-n = 2 and k = 2 (solution : 4) because it's a standard case of the utilisation
- of the power function.
-Same for the following evaluations, which are pretty standard :
-n = 2 and k = 0 (solution : 1)
+The power of two are quite standard, so we will test some of them
 n = 2 and k = 1 (solution : 2)
+n = 2 and k = 2 (solution : 4)
 n = 2 and k = 3 (solution : 8)
+n = 2 and k = 8 (solution : 256)
 
-
-These following tests ensure that if n<0,
+Finally, the following tests ensure that if n<0,
  then the solution is positive if k is even, otherwise the solution is negative.
 n = -2 and k = 0 (solution : 1)
 n = -2 and k = 1 (solution : -2)
@@ -96,31 +93,38 @@ prop_powers n k = and comparisons
 
 -- quickCheck on prop_powers does not fail because we excluded the case k<0
 
+-- In case some tests use prop_powers'
+prop_powers' :: Integer -> Integer -> Bool
 prop_powers' = prop_powers
 
---
+-- This function checks that all power functions are equal
+--  and gives the same result
 powerTest :: Bool
-powerTest = and [prop_powers n k | n <- n_cases, k <- k_cases, elem (n,k) cases]
-            where cases = [
-                           (0,0), (0,1), (0,2), (0,3),
-                           (1,0), (1,1), (1,2), (1,3),
-                           (2,2), ((-2),0), ((-2),1), ((-2),2), ((-2),3), (1,(-1))
-                        ]
-                  n_cases = [0, 1, 2, (-2)]
-                  k_cases = [0, 1, 2, 3, (-1)]
-
---
-powerTest2 :: Bool
-powerTest2 = and [
-                and [prop_powers (n_cases !! ni) (k_cases !! ki),
-                     power (n_cases !! ni) (k_cases !! ki) == cases !! ni !! ki]
-                | ni <- [0..length n_cases - 1], ki <- [0..length k_cases - 1]
+powerTest = and [
+                and [
+                    prop_powers n k,
+                    power n k == v,
+                    prop_powers n (-k)
+                ] | (n, k, v) <- cases
             ]
-          where cases = [
-                            [1, 0, 0, 0],
-                            [1, 1, 1, 1],
-                            [1, 2, 4, 8],
-                            [1, -2, 4, -8]
-                        ]
-                n_cases = [0, 1, 2, (-2)]
-                k_cases = [0, 1, 2, 3]
+            where cases = [
+                            (0, 0, 1),
+                            (0, 1, 0),
+                            (0, 2, 0),
+                            (0, 100, 0),
+
+                            (1, 0, 1),
+                            (1, 1, 1),
+                            (1, 2, 1),
+                            (1, 100, 1),
+
+                            (2, 1, 2),
+                            (2, 2, 4),
+                            (2, 3, 8),
+                            (2, 8, 256),
+
+                            (-2, 0, 1),
+                            (-2, 1, -2),
+                            (-2, 2, 4),
+                            (-2, 3, -8)
+                          ]
