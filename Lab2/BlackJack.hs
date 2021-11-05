19f1bf87d66b8e597e51e2950ed742f3609961a4
Romain <romain.theodet@hotmail.fr>
2021-11-05 11:45:55 +0100

    new: Display hands and cards

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -8,3 +8,20 @@ module BlackJack where
 import Cards
 import RunGame
 import Test.QuickCheck
+
+displaySuit :: Suit -> String
+displaySuit Hearts   = "\9829"
+displaySuit Spades   = "\9824"
+displaySuit Diamonds = "\9830"
+displaySuit Clubs    = "\9827"
+
+displayCard :: Card -> String
+displayCard (Card (Numeric n) s) = show n ++ (displaySuit s)
+displayCard (Card Jack s)        = "J"    ++ (displaySuit s)
+displayCard (Card Queen s)       = "Q"    ++ (displaySuit s)
+displayCard (Card King s)        = "K"    ++ (displaySuit s)
+displayCard (Card Ace s)         = "A"    ++ (displaySuit s)
+
+display :: Hand -> String
+display Empty           = "\n"
+display (Add card hand) = displayCard card ++ " " ++ display hand
