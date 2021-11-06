ffb32f801b2670e8d73a95d19b85aa301a43f5d7
Romain <romain.theodet@hotmail.fr>
2021-11-06 17:49:33 +0100

    change: Less redundant code

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -9,19 +9,28 @@ import Cards
 import RunGame
 import Test.QuickCheck
 
+-- A1
+
+-- This function displays a suit
 displaySuit :: Suit -> String
 displaySuit Hearts   = "\9829"
 displaySuit Spades   = "\9824"
 displaySuit Diamonds = "\9830"
 displaySuit Clubs    = "\9827"
 
-displayCard :: Card -> String
-displayCard (Card (Numeric n) s) = show n ++ (displaySuit s)
-displayCard (Card Jack s)        = "J"    ++ (displaySuit s)
-displayCard (Card Queen s)       = "Q"    ++ (displaySuit s)
-displayCard (Card King s)        = "K"    ++ (displaySuit s)
-displayCard (Card Ace s)         = "A"    ++ (displaySuit s)
+-- This function displays a rank
+displayRank :: Rank -> String
+displayRank (Numeric n) = show n
+displayRank Jack        = "J"
+displayRank Queen       = "Q"
+displayRank King        = "K"
+displayRank Ace         = "A"
 
+-- This function displays a card
+displayCard :: Card -> String
+displayCard (Card r s) = (displayRank r) ++ (displaySuit s)
+
+-- This function displays a hand
 display :: Hand -> String
 display Empty           = "\n"
 display (Add card hand) = displayCard card ++ " " ++ display hand
