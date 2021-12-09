063bdd4f1bd0a1a62230b05dc36dda63aa488732
Fannyrvel <fanny.rouvel@hotmail.fr>
2021-12-09 18:20:54 +0100

    new : Data Type

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,9 +1,19 @@
+import Prelude hiding (sin, cos)
+
+-- Part A
+
+data Function = Sin
+              | Cos
+              deriving (Eq)
+
+data Operator = Add
+              | Mul
+              deriving (Eq)
+
 data Expr = Num Double
           | X
-          | Add Expr Expr
-          | Mul Expr Expr
-          | Sin Expr
-          | Cos Expr
+          | Operator Operator Expr Expr
+          | Function Function Expr
           deriving (Eq)
 
 x :: Expr
@@ -13,21 +23,36 @@ num :: Double -> Expr
 num = Num
 
 add :: Expr -> Expr -> Expr
-add = Add
+add = Operator Add
 
 mul :: Expr -> Expr -> Expr
-mul = Mul
+mul = Operator Mul
 
 sin :: Expr -> Expr
-sin = Sin
+sin = Function Sin
 
 cos :: Expr -> Expr
-cos = Cos
+cos = Function Cos
 
 size :: Expr -> Int
 size (Num _) = 1
 size X = 1
-size (Add e1 e2) = size e1 + size e2
-size (Mul e1 e2) = size e1 + size e2
-size (Sin e) = size e
-size (Cos e) = size e
+size (Operator _ e1 e2) = size e1 + size e2
+size (Function _ e) = size e
+
+-- Part B
+
+instance Show Expr where
+  show = showExpr
+
+showExpr :: Expr -> String
+showExpr (Operator Add e1 e2) = showExpr e1 ++ " + " ++ showExpr e2
+showExpr e                    = showFactor e
+
+showFactor :: Expr -> String
+showFactor (Operator Mul e1 e2) = showFactor e1 ++ " * " ++ showFactor e2
+showFactor (Num n)              = show n
+showFactor (Function Sin e)     = "sin " ++ showExpr e
+showFactor (Function Cos e)     = "cos " ++ showExpr e
+showFactor X                    = "x"
+showFactor e                    = "(" ++ showExpr e ++ ")"
